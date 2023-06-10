549fb083bd6d2fe587352609a143ba760b0de04e
mementomorri <44745297+mementomorri@users.noreply.github.com>
2023-06-10 19:11:59 +0300

    define camera controller

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
 from player import Player, Position
 from conf import PATH_LAND
+from camera import CameraControllerBehaviour
 
 
 class Game(ShowBase):
@@ -11,5 +12,8 @@ class Game(ShowBase):
         ShowBase.__init__(self)
         self.land = Mapmanager()
         x, y, _ = self.land.load_land(PATH_LAND)
-        self.hero = Player(Position(x // 2, y // 2, 2), self.land)
+        self.player = Player(Position(x // 2, y // 2, 2), self.land)
         self.camLens.setFov(90)
+        # cam_controller = CameraControllerBehaviour(self.camera)
+        # cam_controller.setMouseSensivity(0.9)
+        # cam_controller.setup()
